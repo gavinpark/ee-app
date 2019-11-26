1ab49e39d720d550c1d88c8e51b3bed4dea06261
eagadmin <eagadmin@egall-2c4852.wireless.concordia.ca>
2019-11-26 12:53:18 -0500

    added swiper to ArtworkInfoPanel; not mapping images

--- src/Components/ArtworkInfoPanel.js
+++ src/Components/ArtworkInfoPanel.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../App.css';
-import {toggleCopyright, toggleDetailPanel} from '../redux/modules/ui';
+import { toggleCopyright, toggleDetailPanel } from '../redux/modules/ui';
 import CopyrightWindow from './CopyrightWindow.js';
 import { Link } from 'react-router-dom';
+import ArtworkSwiper from './ArtworkSwiper'
+
 
 class ArtworkInfoPanel extends Component {
   render() {
@@ -15,11 +17,12 @@ class ArtworkInfoPanel extends Component {
     console.log("artwork data:", artworkData);
     return (
       <div className="databaseInfo">
-        <div className="infoPanelImage">
-          <img className="artworkImage" src={require(".././images/979_22.jpg")} alt=""></img>
-        </div>
 
-       
+        
+        <ArtworkSwiper />
+        
+
+
         {this.props.isCopyrightOpen && <CopyrightWindow toggleCopyright={this.props.toggleCopyright} />}
         <div className="copyrightButton" onClick={this.props.toggleCopyright}><p>©</p></div>
         <div className="span2Row"></div>
@@ -28,37 +31,37 @@ class ArtworkInfoPanel extends Component {
         <div className="span2Row"></div>
         <div className="span2Row"></div>
         <div className="span2Row"></div>
-        
-        
-          <div className="infoExitButton">
+
+
+        <div className="infoExitButton">
           <Link to="main"><img src={require(".././images/buttons/info_Exit_Button.svg")} alt=""></img></Link>
-            </div>
-      
-    
-          <div className="dataHeader">Artist</div>
-          <div className="dataField">{artworkData.artist}</div>
-          <div className="dataHeader">Title</div>
-          <div className="dataField">{artworkData.title}</div>
-          <div className="dataHeader">Date of production</div>
-          <div className="dataField">{artworkData.date}</div>
-          <div className="dataHeader">Accession Number</div>
-          <div className="dataField">{artworkData.access_num}</div>
-          <div className="dataHeader">Accession Date</div>
-          <div className="dataField">{artworkData.access_date}</div>
-          <div className="dataHeader">Credit</div>
-          <div className="dataField">{artworkData.credit}</div>
-          <div className="dataHeader">Medium</div>
-          <div className="dataField">{artworkData.medium}</div>
-          <div className="dataHeader">Description</div>
-          <div className="dataField">{artworkData.description}</div>
-          <div className="dataHeader">Keywords</div>
-          <div className="dataField">{artworkData.keywords}</div>
-          <div className="dataHeader">Subject</div>
-          <div className="dataField">{artworkData.subject}</div>
-          <div className="dataHeaderReferences">References</div>
-          <div className="dataFieldReferences">{artworkData.references}</div>
-        
-        
+        </div>
+
+
+        <div className="dataHeader">Artist</div>
+        <div className="dataField">{artworkData.artist}</div>
+        <div className="dataHeader">Title</div>
+        <div className="dataField">{artworkData.title}</div>
+        <div className="dataHeader">Date of production</div>
+        <div className="dataField">{artworkData.date}</div>
+        <div className="dataHeader">Accession Number</div>
+        <div className="dataField">{artworkData.access_num}</div>
+        <div className="dataHeader">Accession Date</div>
+        <div className="dataField">{artworkData.access_date}</div>
+        <div className="dataHeader">Credit</div>
+        <div className="dataField">{artworkData.credit}</div>
+        <div className="dataHeader">Medium</div>
+        <div className="dataField">{artworkData.medium}</div>
+        <div className="dataHeader">Description</div>
+        <div className="dataField">{artworkData.description}</div>
+        <div className="dataHeader">Keywords</div>
+        <div className="dataField">{artworkData.keywords}</div>
+        <div className="dataHeader">Subject</div>
+        <div className="dataField">{artworkData.subject}</div>
+        <div className="dataHeaderReferences">References</div>
+        <div className="dataFieldReferences">{artworkData.references}</div>
+
+
         <div className="span2Row"></div>
         <div className="span2Row"></div>
         <div className="span2Row"></div>
@@ -98,7 +101,7 @@ class ArtworkInfoPanel extends Component {
         <div className="span2Row"></div>
         <div className="span2Row"></div>
 
-   
+
 
 
 
---
+++ src/Components/ArtworkSwiper.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Swiper from 'react-id-swiper';
+import 'swiper/css/swiper.css';
+import '../App.css';
+
+
+class ArtworkSwiper extends Component {
+    render() {
+// TODO: PASS PARAMS AND MAP INTO SWIPER
+        return (
+            <div className="infoPanelImage">
+                <Swiper>
+                    <img className="artworkImage" src={require(".././images/979_22.jpg")} alt=""></img>
+                    <div>Slide 2</div>
+                    <div>Slide 3</div>
+                </Swiper>
+            </div>
+        );
+
+    }
+}
+
+export default ArtworkSwiper;
