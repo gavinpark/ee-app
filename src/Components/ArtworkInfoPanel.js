60a78c8a2fc5387bb0455141a16bab39294bb78c
eagadmin <eagadmin@egall-2c46b9.concordia.ca>
2019-12-10 15:22:51 -0500

    conditions for table rows

--- src/Components/ArtworkInfoPanel.js
+++ src/Components/ArtworkInfoPanel.js
@@ -8,6 +8,79 @@ import ArtworkSwiper from './ArtworkSwiper'
 
 
 class ArtworkInfoPanel extends Component {
+  determineTitleRowSize(artworkData){
+    if(artworkData.title.length > 300){
+      return("span4-row")
+    }
+    if((artworkData.title.length > 201) && (artworkData.title.length < 300)){
+      return ("span3-row")
+    }
+    if((artworkData.title.length > 100) && (artworkData.title.length < 200)){
+      return("span2-row")
+    }
+    else{
+      return("")
+    }
+  }
+  
+  determineCreditRowSize(artworkData){
+    if(artworkData.credit.length > 300){
+      return("span4-row")
+    }
+    if((artworkData.credit.length > 201) && (artworkData.credit.length < 300)){
+      return ("span3-row")
+    }
+    if((artworkData.credit.length > 100) && (artworkData.credit.length < 200)){
+      return("span2-row")
+    }
+    else{
+      return ("")
+    }
+  }
+
+  determineKeyRowSize(artworkData){
+    if(artworkData.keywords.length > 300){
+      return("span4-row")
+    }
+    if((artworkData.keywords.length > 201) && (artworkData.keywords.length < 300)){
+      return ("span3-row")
+    }
+    if((artworkData.keywords.length > 100) && (artworkData.keywords.length < 200)){
+      return("span2-row")
+    }
+    else{
+      return("")
+    }
+  }
+  determineSubjectRowSize(artworkData){
+    if(artworkData.subject.length > 300){
+      return("span4-row")
+    }
+    if((artworkData.subject.length > 201) && (artworkData.subject.length < 300)){
+      return ("span3-row")
+    }
+    if((artworkData.subject.length > 100) && (artworkData.subject.length < 200)){
+      return("span2-row")
+    }
+    else{
+      return("")
+    }
+  }
+  determineRefRowSize(artworkData){
+    if(artworkData.references.length > 300){
+      return("span4-row")
+    }
+    if((artworkData.references.length > 201) && (artworkData.references.length < 300)){
+      return ("span3-row")
+    }
+    if((artworkData.references.length > 100) && (artworkData.references.length < 200)){
+      return("span2-row")
+    }
+    else{
+      return("span2-row")
+    }
+  }
+
   hasRights(artworkData) {
 
     console.log('hello artworkData', artworkData)
@@ -44,8 +117,8 @@ render() {
         <th>Artist</th>
         <td>{artworkData.artist}</td>
 
-        <th>Title</th>
-        <td>{artworkData.title}</td>
+        <th className={this.determineTitleRowSize(artworkData)}>Title</th>
+        <td className={this.determineTitleRowSize(artworkData)}>{artworkData.title}</td>
 
         <th>Date of Production</th>
         <td>{artworkData.date}</td>
@@ -62,23 +135,24 @@ render() {
         <th>Status</th>
         <td>{artworkData.status}</td>
 
-        <th>Credit</th>
-        <td>{artworkData.credit}</td>
+        <th className={this.determineCreditRowSize(artworkData)}>Credit</th>
+        <td className={this.determineCreditRowSize(artworkData)}>{artworkData.credit}</td>
 
         <th>Medium</th>
         <td>{artworkData.medium}</td>
 
         <th>Description</th>
         <td>{artworkData.description}</td>
+  
 
-        <th>Keywords</th>
-        <td>{artworkData.keywords}</td>
+        <th className={this.determineKeyRowSize(artworkData)}>Keywords</th>
+        <td className={this.determineKeyRowSize(artworkData)}>{artworkData.keywords}</td>
 
-        <th>Subject</th>
-        <td>{artworkData.subject}</td>
+        <th className={this.determineSubjectRowSize(artworkData)}>Subject</th>
+        <td className={this.determineSubjectRowSize(artworkData)}>{artworkData.subject}</td>
 
-        <th>References</th>
-        <td>{artworkData.references}</td>
+        <th className={this.determineRefRowSize(artworkData)}>References</th>
+        <td className={this.determineRefRowSize(artworkData)}>{artworkData.references}</td>
 
 
       </table>
