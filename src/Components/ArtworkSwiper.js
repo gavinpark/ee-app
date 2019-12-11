8f78341431e7e272a93fbc788c70c5b77687a5ef
krustina <kristinavannan@gmail.com>
2019-12-10 20:56:39 -0500

    added params to swiper..but not rly

--- src/Components/ArtworkSwiper.js
+++ src/Components/ArtworkSwiper.js
@@ -5,12 +5,19 @@ import '../App.css';
 
 
 class ArtworkSwiper extends Component {
+    
     getSwiper() {
+            const params = {
+              navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+              },
+            }
         if (this.props.link.length >= 2) {
             {
                 return (this.props.link.map((image) => {
                     console.log('images', image);
-                    return <Swiper><img draggable="false" className="artworkImage" src={'http://ellengallery.concordia.ca/resi/images/' + image} alt=""></img></Swiper>
+                    return <Swiper {...params}><img draggable="false" className="artworkImage" src={'http://ellengallery.concordia.ca/resi/images/' + image} alt=""></img></Swiper>
 
                 })
 
@@ -23,7 +30,6 @@ class ArtworkSwiper extends Component {
         }
     }
     render() {
-        // TODO: PASS PARAMS AND MAP INTO SWIPER
         return (
             <div className="infoPanelImage">
 
