a9db6a38ced01e24eacc3c9864a60f54fa945ff5
eagadmin <eagadmin@egall-2c4852.wireless.concordia.ca>
2019-12-11 09:53:40 -0500

    finished swiper

--- src/Components/ArtworkSwiper.js
+++ src/Components/ArtworkSwiper.js
@@ -5,24 +5,24 @@ import '../App.css';
 
 
 class ArtworkSwiper extends Component {
-    
+
     getSwiper() {
-            const params = {
-              navigation: {
+        const params = {
+            navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev'
-              },
-            }
+            },
+        }
         if (this.props.link.length >= 2) {
-            {
-                return (this.props.link.map((image) => {
+            return (<Swiper {...params}>
+                {this.props.link.map((image) => {
                     console.log('images', image);
-                    return <Swiper {...params}><img draggable="false" className="artworkImage" src={'http://ellengallery.concordia.ca/resi/images/' + image} alt=""></img></Swiper>
+                    return <img draggable="false" className="artworkImage" src={'http://ellengallery.concordia.ca/resi/images/' + image} alt=""></img>
 
-                })
+                }
 
                 )
-            }
+                }</Swiper>)
         } else {
             return (
                 <img draggable="false" className="artworkImage" src={'http://ellengallery.concordia.ca/resi/images/' + this.props.link[0]} alt=""></img>
@@ -34,7 +34,7 @@ class ArtworkSwiper extends Component {
             <div className="infoPanelImage">
 
                 {this.getSwiper()}
-                
+
             </div >
         );
     }
