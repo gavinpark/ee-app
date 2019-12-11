a8acfd66efd97bed0edf43be1e6fcd79353109e7
eagadmin <eagadmin@egall-2c46b9.concordia.ca>
2019-12-11 10:22:17 -0500

    added if statements for dimensions

--- src/Components/ArtworkInfoPanel.js
+++ src/Components/ArtworkInfoPanel.js
@@ -81,6 +81,41 @@ class ArtworkInfoPanel extends Component {
     }
   }
 
+  determineHeight(artworkData){
+    if(artworkData.height === "") {
+      return ("")
+    }
+    else{
+      return(artworkData.height + "cm")
+    }
+  }
+  determineLength(artworkData){
+    if(artworkData.length === "") {
+      return ("")
+    }
+    else{
+      return('\xa0' + "x" + '\xa0' + artworkData.length + "cm")
+    }
+  }
+
+  determineWidth(artworkData){
+    if(artworkData.width === "") {
+      return ("")
+    }
+    else{
+      return('\xa0' + "x" + '\xa0' + artworkData.width + "cm")
+    }
+  }
+
+  determineDepth(artworkData){
+    if(artworkData.depth === "") {
+      return ("")
+    }
+    else{
+      return('\xa0' + "x" + '\xa0' + artworkData.depth + "cm")
+    }
+  }
+
   hasRights(artworkData) {
 
     console.log('hello artworkData', artworkData)
@@ -130,7 +165,7 @@ render() {
         <td>{artworkData.access_date}</td>
 
         <th>Dimensions</th>
-        <td>{artworkData.length}cm</td>
+        <td>{this.determineHeight(artworkData)}{this.determineLength(artworkData)}{this.determineWidth(artworkData)}{this.determineDepth(artworkData)}</td>
 
         <th>Status</th>
         <td>{artworkData.status}</td>
