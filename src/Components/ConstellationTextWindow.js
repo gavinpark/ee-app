9f28160b872844628b719d86749dab9b3715e385
eagadmin <eagadmin@egall-2c4852.wireless.concordia.ca>
2019-12-12 12:45:27 -0500

    removed/commented out the french toggle

--- src/Components/ConstellationTextWindow.js
+++ src/Components/ConstellationTextWindow.js
@@ -4,9 +4,16 @@ import { connect } from 'react-redux';
 import EssayData from '.././data/EssayData.js';
 import '../App.css';
 import { Rnd } from "react-rnd";
-import { toggleLanguage, toggleConstellationText, increaseHighestZIndex } from '../redux/modules/ui';
+import { toggleConstellationText, increaseHighestZIndex } from '../redux/modules/ui';
+// toggleLanguage, 
 
-// TODO be able to remember language state when opening multiple windows
+
+// *** TO RE-AD LANGUAGE FEAT: 
+// 1. Uncomment toggleLanguage in import, and mapStatetoProps
+// 2. Uncomment getLanguage() and replace html inside rnd render with call to function
+// 3. I did not comment out anything in ui.js..so that should be fine
+// 4. Double check that everywhere is turned back on: Welcome Window, Constellation Text Window, Copyright Window and Essay Window
+// 5. look out for props in essay to make sure its pulling from the right spot
 
 class ConstellationTextWindow extends Component {
     constructor(props) {
@@ -22,51 +29,51 @@ class ConstellationTextWindow extends Component {
         })
         this.props.increaseHighestZIndex();
     }
-    getLanguage() {
-        if (this.props.isFrench) {
-            return (
-                <div>
-                    <div className="essayHeaderBox">
-                        <div className="essayHeader">{EssayData[2].essayHeader}</div>
-                    </div>
-                    <div>
-                        <div className="objectENButton">EN</div>
-                        <div className="objectFRButton greyOut" onClick={this.props.toggleLanguage}>FR</div>
-                    </div>
+    // getLanguage() {
+    //     if (this.props.isFrench) {
+    //         return (
+    //             <div>
+    //                 <div className="essayHeaderBox">
+    //                     <div className="essayHeader">{EssayData[2].essayHeader}</div>
+    //                 </div>
+    //                 <div>
+    //                     <div className="objectENButton">EN</div>
+    //                     <div className="objectFRButton greyOut" onClick={this.props.toggleLanguage}>FR</div>
+    //                 </div>
 
-                    <img
-                        className="objectExitButton"
-                        src={require(".././images/buttons/exit_Button.svg")}
-                        alt=""
-                        onClick={this.props.toggleConstellationText}
-                    ></img>
-                    <div className="essayBodyBox">
-                        <div className="essayBody">{EssayData[2].essayText}</div>
-                    </div>
-                </div>
-            )
-        } return (
-            <div>
-                <div className="essayHeaderBox">
-                    <div className="essayHeader">{EssayData[2].essayHeaderFR}</div>
-                </div>
-                <div>
-                    <div className="objectENButton greyOut" onClick={this.props.toggleLanguage}>EN</div>
-                    <div className="objectFRButton">FR</div>
-                </div>
+    //                 <img
+    //                     className="objectExitButton"
+    //                     src={require(".././images/buttons/exit_Button.svg")}
+    //                     alt=""
+    //                     onClick={this.props.toggleConstellationText}
+    //                 ></img>
+    //                 <div className="essayBodyBox">
+    //                     <div className="essayBody">{EssayData[2].essayText}</div>
+    //                 </div>
+    //             </div>
+    //         )
+    //     } return (
+    //         <div>
+    //             <div className="essayHeaderBox">
+    //                 <div className="essayHeader">{EssayData[2].essayHeaderFR}</div>
+    //             </div>
+    //             <div>
+    //                 <div className="objectENButton greyOut" onClick={this.props.toggleLanguage}>EN</div>
+    //                 <div className="objectFRButton">FR</div>
+    //             </div>
 
-                <img
-                    className="objectExitButton"
-                    src={require(".././images/buttons/exit_Button.svg")}
-                    alt=""
-                    onClick={this.props.toggleConstellationText}
-                ></img>
-                <div className="essayBodyBox">
-                    <div className="essayBody">{EssayData[2].essayTextFR}</div>
-                </div>
-            </div>
-        )
-    }
+    //             <img
+    //                 className="objectExitButton"
+    //                 src={require(".././images/buttons/exit_Button.svg")}
+    //                 alt=""
+    //                 onClick={this.props.toggleConstellationText}
+    //             ></img>
+    //             <div className="essayBodyBox">
+    //                 <div className="essayBody">{EssayData[2].essayTextFR}</div>
+    //             </div>
+    //         </div>
+    //     )
+    // }
     render() {
         return (
             <Rnd
@@ -82,7 +89,21 @@ class ConstellationTextWindow extends Component {
             // style={{overflow: "scroll"}}
             >
                 <div onClick={this.bringItemToHighestZIndex}>
-                    {this.getLanguage()}
+                    {/* {this.getLanguage()} */}
+                    <div className="essayHeaderBox">
+                        <div className="essayHeader">{EssayData[2].essayHeader}</div>
+                    </div>
+
+                    <img
+                        className="objectExitButton"
+                        src={require(".././images/buttons/exit_Button.svg")}
+                        alt=""
+                        onClick={this.props.toggleConstellationText}
+                    ></img>
+                    <div className="essayBodyBox">
+                        <div className="essayBody">{EssayData[2].essayText}</div>
+
+                    </div>
                 </div>
             </Rnd>
         );
@@ -91,12 +112,13 @@ class ConstellationTextWindow extends Component {
 }
 const mapStateToProps = (state) => {
     return {
-        isFrench: state._ui.isFrench,
+        // isFrench: state._ui.isFrench,
         highestZIndex: state._ui.highestZIndex,
         isConstellationTextOpen: state._ui.isConstellationTextOpen
     };
 };
 
 export default connect(mapStateToProps, {
-    toggleLanguage, increaseHighestZIndex, toggleConstellationText
+    // toggleLanguage, 
+    increaseHighestZIndex, toggleConstellationText
 })(ConstellationTextWindow);
--- src/Components/WelcomeWindow.js
+++ src/Components/WelcomeWindow.js
@@ -4,10 +4,15 @@ import { connect } from 'react-redux';
 import EssayData from '.././data/EssayData.js';
 import '../App.css';
 import { Rnd } from "react-rnd";
-import { toggleLanguage, increaseHighestZIndex } from '../redux/modules/ui';
+import { increaseHighestZIndex } from '../redux/modules/ui';
+// toggleLanguage, 
 
-// TODO be able to remember language state when opening multiple windows
-
+// *** TO RE-AD LANGUAGE FEAT: 
+// 1. Uncomment toggleLanguage in import, and mapStatetoProps
+// 2. Uncomment getLanguage() and replace html inside rnd render with call to function
+// 3. I did not comment out anything in ui.js..so that should be fine
+// 4. Double check that everywhere is turned back on: Welcome Window, Constellation Text Window, Copyright Window and Essay Window
+// 5. look out for props in essay to make sure its pulling from the right spot
 
 class WelcomeWindow extends Component {
     constructor(props) {
@@ -27,59 +32,59 @@ class WelcomeWindow extends Component {
         })
         this.props.increaseHighestZIndex();
     }
-    getLanguage() {
-        if (this.props.isFrench) {
-            return (
-                <div>
-                    <div className="essayHeaderBox">
-                        <div className="essayHeader">{EssayData[0].essayHeader}</div>
-                    </div>
-                    <div>
-                        <div className="objectENButton">EN</div>
-                        <div className="objectFRButton greyOut" onClick={this.props.toggleLanguage}>FR</div>
-                    </div>
+    // getLanguage() {
+    //     if (this.props.isFrench) {
+    //         return (
+    //             <div>
+    //                 <div className="essayHeaderBox">
+    //                     <div className="essayHeader">{EssayData[0].essayHeader}</div>
+    //                 </div>
+    //                 <div>
+    //                     <div className="objectENButton">EN</div>
+    //                     <div className="objectFRButton greyOut" onClick={this.props.toggleLanguage}>FR</div>
+    //                 </div>
 
-                    <img
-                        className="objectExitButton"
-                        src={require(".././images/buttons/exit_Button.svg")}
-                        alt=""
-                        onClick={this.props.toggleWelcome}
-                    ></img>
-                    <div className="essayBodyBox">
-                        <div className="essayBody">{EssayData[0].essayText}</div>
-                    </div>
-                    <div className="essayFootnoteBox">
-                        <div className="footnoteEnd">{EssayData[0].footnote}</div>
-                    </div>
-                </div>
-            )
-        } return (
-            <div>
-                <div className="essayHeaderBox">
-                    <div className="essayHeader">{EssayData[0].essayHeaderFR}</div>
-                </div>
-                <div>
-                    <div className="objectENButton greyOut" onClick={this.props.toggleLanguage}>EN</div>
-                    <div className="objectFRButton">FR</div>
-                </div>
+    //                 <img
+    //                     className="objectExitButton"
+    //                     src={require(".././images/buttons/exit_Button.svg")}
+    //                     alt=""
+    //                     onClick={this.props.toggleWelcome}
+    //                 ></img>
+    //                 <div className="essayBodyBox">
+    //                     <div className="essayBody">{EssayData[0].essayText}</div>
+    //                 </div>
+    //                 <div className="essayFootnoteBox">
+    //                     <div className="footnoteEnd">{EssayData[0].footnote}</div>
+    //                 </div>
+    //             </div>
+    //         )
+    //     } return (
+    //         <div>
+    //             <div className="essayHeaderBox">
+    //                 <div className="essayHeader">{EssayData[0].essayHeaderFR}</div>
+    //             </div>
+    //             <div>
+    //                 <div className="objectENButton greyOut" onClick={this.props.toggleLanguage}>EN</div>
+    //                 <div className="objectFRButton">FR</div>
+    //             </div>
 
-                <img
-                    className="objectExitButton"
-                    src={require(".././images/buttons/exit_Button.svg")}
-                    alt=""
-                    onClick={this.props.toggleWelcome}
-                ></img>
-                <div className="essayBodyBox">
-                    <div className="essayBody">{EssayData[0].essayTextFR}</div>
+    //             <img
+    //                 className="objectExitButton"
+    //                 src={require(".././images/buttons/exit_Button.svg")}
+    //                 alt=""
+    //                 onClick={this.props.toggleWelcome}
+    //             ></img>
+    //             <div className="essayBodyBox">
+    //                 <div className="essayBody">{EssayData[0].essayTextFR}</div>
 
-                </div>
-                <div className="downloadBox">Download Full Text</div>
-                <div className="essayFootnoteBox">
-                    <div className="footnoteEnd">{EssayData[0].footnoteFR}</div>
-                </div>
-            </div>
-        )
-    }
+    //             </div>
+    // <div className="downloadBox">Download Full Text</div>
+    // <div className="essayFootnoteBox">
+    //     <div className="footnoteEnd">{EssayData[0].footnoteFR}</div>
+    // </div>
+    //         </div>
+    //     )
+    // }
 
     render() {
         return (
@@ -96,7 +101,25 @@ class WelcomeWindow extends Component {
             // style={{overflow: "scroll"}}
             >
                 <div onClick={this.bringItemToHighestZIndex}>
-                    {this.getLanguage()}
+                    {/* {this.getLanguage()} */}
+                    <div className="essayHeaderBox">
+                        <div className="essayHeader">{EssayData[0].essayHeader}</div>
+                    </div>
+
+                    <img
+                        className="objectExitButton"
+                        src={require(".././images/buttons/exit_Button.svg")}
+                        alt=""
+                        onClick={this.props.toggleWelcome}
+                    ></img>
+                    <div className="essayBodyBox">
+                        <div className="essayBody">{EssayData[0].essayText}</div>
+
+                    </div>
+                    <div className="downloadBox">Download Full Text</div>
+                    <div className="essayFootnoteBox">
+                        <div className="footnoteEnd">{EssayData[0].footnote}</div>
+                    </div>
                 </div>
             </Rnd>
         );
@@ -105,11 +128,12 @@ class WelcomeWindow extends Component {
 }
 const mapStateToProps = (state) => {
     return {
-        isFrench: state._ui.isFrench,
+        // isFrench: state._ui.isFrench,
         highestZIndex: state._ui.highestZIndex,
     };
 };
 
 export default connect(mapStateToProps, {
-    toggleLanguage, increaseHighestZIndex
+    // toggleLanguage, 
+    increaseHighestZIndex
 })(WelcomeWindow);
