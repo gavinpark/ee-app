a50ae880cd063e6d333faecaf06a02d67afa0c1d
eagadmin <eagadmin@egall-2c4852.wireless.concordia.ca>
2019-12-16 14:47:57 -0500

    latest changes

--- src/Components/ArtworkInfoPanel.js
+++ src/Components/ArtworkInfoPanel.js
@@ -132,16 +132,17 @@ class ArtworkInfoPanel extends Component {
   hasRights(artworkData) {
     if (artworkData.have_rights === "Oui") {
       return (<ArtworkSwiper link={artworkData.link} {...this.props} />)
-    }else{
-      return("")
     }
-    // if(artworkData.have_rights === "Non" || artworkData.have_rights === ""){
-    //   if(this.props.isCopyrightOpen){
-    //     return("")
-    //   }else{
-    //     return(<CopyrightWindow toggleCopyright={this.props.toggleCopyright} />)
-    //   }
+    // else{
+    //   return("")
     // }
+    if(artworkData.have_rights === "Non" || artworkData.have_rights === ""){
+      if(this.props.isCopyrightOpen){
+        return("")
+      }else{
+        return(<CopyrightWindow toggleCopyright={this.props.toggleCopyright} />)
+      }
+    }
   }
   getMedium(artworkData) {
     if (artworkData.medium && artworkData.support) {
