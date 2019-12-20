4f106c31e86537bcfd2faf8f82b93aec2ab0ab7c
krustina <kristinavannan@gmail.com>
2019-12-20 13:24:17 -0600

    fixed drag on keywords

--- src/Components/ConstellationKeyword.js
+++ src/Components/ConstellationKeyword.js
@@ -6,82 +6,88 @@ import { increaseHighestZIndex, onHoverKeyword, offHoverKeyword } from '../redux
 
 class ConstellationKeyword extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          randomX: null,
-          randomY: null,
-          zIndex: 0,
-        }
-      }
-      componentDidMount() {
-        this.getRandomXPosition();
-        this.getRandomYPosition();
-        // this.movezIndexToTop();
-      }
-
-      handleMouseOver = () => {
-        this.props.onHoverKeyword(this.props.keyword)
-      }
-
-      handleMouseOff = () => {
-        this.props.offHoverKeyword()
-      }
-
-      bringItemToHighestZIndex = () => {
-        const nextHighestZindex = this.props.highestZIndex + 1;
-        this.setState({
-          zIndex: nextHighestZindex,
-        })
-        this.props.increaseHighestZIndex();
-      }
-      getRandomXPosition(){
-        var min = 0;
-        var x = document.getElementsByClassName('constellationPanel')[0].offsetHeight;
-        var randomX = Math.floor(Math.random() * (x - min)) + min;
-        // return randomX;
-        this.setState({
-          randomX,
-        });
-      }
-      getRandomYPosition(){
-        var min = 0;
-        var y = document.getElementsByClassName('constellationPanel')[0].offsetWidth;
-        var randomY = Math.floor(Math.random() * (y - min)) + min;
-        // return randomY;
-        this.setState({
-          randomY,
-        });
-      }
-    render() {
-      const word = this.props.keyword;
-      const isRelatedToHoveredArtworkClass = this.props.isRelatedToHoveredArtwork
-      ? 'isRelatedToHoveredArtwork' :
-      this.props.isArtworkHovered ? 'isNotRelatedToHoveredArtwork' : '';
-        return (
-            <Rnd
-                enableResizing={null}
-                style={{ zIndex: this.state.zIndex }}
-                position={{
-                    x: this.state.randomX,
-                    y: this.state.randomY,
-                }}
-            >
-                <div
-                  className={`${isRelatedToHoveredArtworkClass} keyword`}
-                  key={word}
-                  onClick={this.bringItemToHighestZIndex}
-                  onMouseOver={this.handleMouseOver}
-                  onMouseOut={this.handleMouseOff}
-                >{word}</div>
-            </Rnd>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomX: null,
+      randomY: null,
+      zIndex: 0,
     }
+  }
+  componentDidMount() {
+    this.getRandomXPosition();
+    this.getRandomYPosition();
+    // this.movezIndexToTop();
+  }
+
+  handleMouseOver = () => {
+    this.props.onHoverKeyword(this.props.keyword)
+  }
+
+  handleMouseOff = () => {
+    this.props.offHoverKeyword()
+  }
+
+  bringItemToHighestZIndex = () => {
+    const nextHighestZindex = this.props.highestZIndex + 1;
+    this.setState({
+      zIndex: nextHighestZindex,
+    })
+    this.props.increaseHighestZIndex();
+  }
+  getRandomXPosition() {
+    var min = 0;
+    var x = document.getElementsByClassName('constellationPanel')[0].offsetHeight;
+    var randomX = Math.floor(Math.random() * (x - min)) + min;
+    // return randomX;
+    this.setState({
+      randomX,
+    });
+  }
+  getRandomYPosition() {
+    var min = 0;
+    var y = document.getElementsByClassName('constellationPanel')[0].offsetWidth;
+    var randomY = Math.floor(Math.random() * (y - min)) + min;
+    // return randomY;
+    this.setState({
+      randomY,
+    });
+  }
+  render() {
+    if (this.state.randomX && this.state.randomY) {
+      const word = this.props.keyword
+      const isRelatedToHoveredArtworkClass = this.props.isRelatedToHoveredArtwork
+        ? 'isRelatedToHoveredArtwork' :
+        this.props.isArtworkHovered ? 'isNotRelatedToHoveredArtwork' : '';
+      return (
+        <Rnd
+          enableResizing={null}
+          style={{ zIndex: this.state.zIndex }}
+          default={{
+            x: this.state.randomX,
+            y: this.state.randomY,
+          }}
+        >
+          <div
+            className={`${isRelatedToHoveredArtworkClass} keyword`}
+            key={word}
+            onClick={this.bringItemToHighestZIndex}
+            onMouseOver={this.handleMouseOver}
+            onMouseOut={this.handleMouseOff}
+          >{word}</div>
+        </Rnd>
+      )
+    }else{
+      return null
+    }
+
+  }
+
 }
 const mapStateToProps = (state) => {
   return {
-      highestZIndex: state._ui.highestZIndex,
-      isArtworkHovered: state._ui.isArtworkHovered,
+    highestZIndex: state._ui.highestZIndex,
+    isArtworkHovered: state._ui.isArtworkHovered,
   };
 };
 
