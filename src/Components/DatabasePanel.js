a8c27d742b38b4e72c8e2461e4a4ebaeb5c340d4
Gavin <gavin.e.park@gmail.com>
2020-01-02 10:48:34 -0500

    latest changes

--- src/Components/DatabasePanel.js
+++ src/Components/DatabasePanel.js
@@ -10,6 +10,14 @@ import { addWorkToConstellation, shuffle } from '../redux/modules/ui';
 // new fieldnames are 
 // access_num,access_date,artist,title,date,keywords,subject,color,medium,description,object_name,technique,have_rights,rights_holder,credit,object_rights,birthdate,deathdate,note,link,references,source,status,acquisition_mode
 
+const generateEmpties = (numberOfEmpties) => {
+  const emptiesArray = new Array(numberOfEmpties);
+  const empties = emptiesArray.fill("", 0, numberOfEmpties);
+  return empties.map(empty => {
+    return <div>{empty}</div>;
+  });
+}
+
 class DatabasePanel extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -32,8 +40,28 @@ class DatabasePanel extends Component {
         index,
       }
     });
-    
+
     const finalArray = shuffle([...databaseItemsArray, ...essayArray]);
+    let viewportNumberofColumnBlocks = 5;
+    let numBufferFillIns = 45;
+    const windowWidth = window.innerWidth;
+    if (windowWidth >= 1200) {
+      viewportNumberofColumnBlocks = 5;
+      numBufferFillIns = 45;
+    } else if (windowWidth <= 1200 && windowWidth >= 900) {
+      viewportNumberofColumnBlocks = 3;
+      numBufferFillIns = 27;
+    } else if (windowWidth <= 900 && windowWidth >= 600) {
+      viewportNumberofColumnBlocks = 3;
+      numBufferFillIns = 18;
+    } else if (windowWidth <= 600 && windowWidth >= 0) {
+      viewportNumberofColumnBlocks = 2;
+      numBufferFillIns = 6;
+    }
+    const bufferFillIns = generateEmpties(numBufferFillIns);
+    console.log('DATABASEnumBufferFillIns', numBufferFillIns);
+    console.log('DATABASEBufferFillIns', bufferFillIns);
+    console.log('DATABASEviewportNumberofColumnBlocks', viewportNumberofColumnBlocks);
 
     return (
       <div className="databasePanel">
@@ -44,12 +72,13 @@ class DatabasePanel extends Component {
             const artworkData = window.allWorks[artwork.access_num];
             return <DatabaseItem artworkData={artworkData} artwork={artwork} {...this.props}></DatabaseItem>
           }
-          if (obj.type === 'essay'){
+          if (obj.type === 'essay') {
             const essay = obj.data;
-           
+
             return <EssayButton essay={essay} index={obj.index} {...this.props}> </EssayButton>
           }
         })}
+        {bufferFillIns}
 
       </div>
     );
