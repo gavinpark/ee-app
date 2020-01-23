2f87d2f18c20a1753beaa237ad6a0c700db01fc1
krustina <kristinavannan@gmail.com>
2020-01-23 14:07:58 -0500

    copyright french toggle added

--- src/Components/CopyrightWindow.js
+++ src/Components/CopyrightWindow.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 import EssayData from '.././data/EssayData.js';
 import '../App.css';
 import { Rnd } from "react-rnd";
-import { toggleCopyright } from '../redux/modules/ui';
-// toggleLanguage, 
+import { toggleCopyright, toggleLanguage } from '../redux/modules/ui';
 
 // *** TO RE-AD LANGUAGE FEAT: 
 // 1. Uncomment toggleLanguage in import, and mapStatetoProps
@@ -14,107 +13,121 @@ import { toggleCopyright } from '../redux/modules/ui';
 // 5. look out for props in essay to make sure its pulling from the right spot
 
 class CopyrightWindow extends Component {
+    getLanguage() {
+        if (this.props.isFrench) {
+            return (
+                // <div>
+                    <div className="essayHeaderBox">
+                        <h1 className="essayHeader">{EssayData[6].essayHeaderFR}</h1>
+                    {/* </div> */}
+                    <div>
+                        <div className="objectENButton greyOut cursorPoint" onClick={this.props.toggleLanguage}>EN</div>
+                        <div className="objectFRButton cursorPoint">FR</div>
+                    </div>
+                    <img
+                        className="objectExitButton cursorPoint"
+                        src="http://ellengallery.concordia.ca/establishing-a-database-connection/static/media/exit_Button.svg"
+                        alt=""
+                        onClick={this.props.toggleCopyright}
+                    ></img>
+                    <div className="copyrightBodyBox">
+                        <div className="essayBody">
+                            <p><i>{EssayData[6].disclaimerFR}</i></p>
+                            <br />
+                            <p>__________</p>
+                            <br />
+                            <p>{EssayData[6].essayTextFR[0]}</p>
 
-    // getLanguage() {
-    //     if (this.props.isFrench) {
-    //         return (
-    //             <div>
-    //                 <div className="essayHeaderBox">
-    //                     <div className="essayHeader">{EssayData[1].essayHeader}</div>
-    //                 </div>
-    //                 <div>
-    //                     <div className="objectENButton">EN</div>
-    //                     <div className="objectFRButton greyOut" onClick={this.props.toggleLanguage}>FR</div>
-    //                 </div>
+                            <br />
+                            <p>{EssayData[6].essayTextFR[1]}</p>
 
-    //                 <img
-    //                     className="objectExitButton"
-    //                     src="http://ellengallery.concordia.ca/establishing-a-database-connection/static/media/exit_Button.svg"
-    //                     alt=""
-    //                     onClick={this.props.toggleCopyright}
-    //                 ></img>
-    //                 <div className="essayBodyBox">
-    //                     <div className="essayBody">{EssayData[0].essayText}</div>
-    //                 </div>
-    //             </div>
-    //         )
-    //     } return (
-    //         <div>
-    //             <div className="essayHeaderBox">
-    //                 <div className="essayHeader">{EssayData[0].essayHeaderFR}</div>
-    //             </div>
-    //             <div>
-    //                 <div className="objectENButton greyOut" onClick={this.props.toggleLanguage}>EN</div>
-    //                 <div className="objectFRButton">FR</div>
-    //             </div>
+                            <br />
+                            <p>{EssayData[6].essayTextFR[2]}</p>
 
-    //             <img
-    //                 className="objectExitButton"
-    //                 src="http://ellengallery.concordia.ca/establishing-a-database-connection/static/media/exit_Button.svg"
-    //                 alt=""
-    //                 onClick={this.props.toggleCopyright}
-    //             ></img>
-    //             <div className="essayBodyBox">
-    //                 <div className="essayBody">{EssayData[0].essayTextFR}</div>
-    //             </div>
-    //         </div>
-    //     )
-    // }
+                            <br />
+                            <p>{EssayData[6].essayTextFR[3]}</p>
+
+                            <a
+                                className="downloadBox cursorPoint"
+                                href="http://ellengallery.concordia.ca/establishing-a-database-connection/static/media/EDC_Essay.pdf"
+                                target="_blank">
+                                Afficher le texte complet </a>
+
+                        </div>
+
+                    </div>
+                    <div className="essayFootnoteBox">
+                        <p>{EssayData[6].footnoteFR[0]}</p>
+
+                        <p>{EssayData[6].footnoteFR[1]}</p>
+
+                        <p>{EssayData[6].footnoteFR[2]}</p>
+                        <div className="whiteOverlay"></div>
+                    </div>
+                </div>
+            )
+
+        } else {
+            return (
+                // <div>
+                    <div className="essayHeaderBox">
+                        <h1 className="essayHeader ">{EssayData[6].essayHeader}</h1>
+                    {/* </div> */}
+                    <div>
+                        <div className="objectENButton cursorPoint">EN</div>
+                        <div className="objectFRButton greyOut cursorPoint" onClick={this.props.toggleLanguage}>FR</div>
+                    </div>
+                    <img
+                        className="objectExitButton cursorPoint"
+                        src="http://ellengallery.concordia.ca/establishing-a-database-connection/static/media/exit_Button.svg"
+                        alt=""
+                        onClick={this.props.toggleCopyright}
+                    ></img>
+                    <div className="copyrightBodyBox">
+                        <div className="essayBody">
+                            <p><i>{EssayData[6].disclaimer}</i></p>
+                            <br />
+                            <p>__________</p>
+                            <br />
+                            <p>{EssayData[6].essayText[0]}</p>
+
+                            <br />
+                            <p>{EssayData[6].essayText[1]}</p>
+
+                            <br />
+                            <p>{EssayData[6].essayText[2]}</p>
+
+                            <br />
+                            <p>{EssayData[6].essayText[3]}</p>
+
+                            <a
+                                className="downloadBox cursorPoint"
+                                href="http://ellengallery.concordia.ca/establishing-a-database-connection/static/media/EDC_Essay.pdf"
+                                target="_blank">
+                                View Full Text </a>
+
+                        </div>
+
+                    </div>
+                    <div className="essayFootnoteBox">
+                        <p>{EssayData[6].footnote[0]}</p>
+
+                        <p>{EssayData[6].footnote[1]}</p>
+
+                        <p>{EssayData[6].footnote[2]}</p>
+                        <div className="whiteOverlay"></div>
+                    </div>
+                </div>
+            )
+        }
+    }
 
     render() {
 
         return (
-                <div
-                    className="copyrightContainer">
-                    <div>
-                        {/* {this.getLanguage()} */}
-                        <div className="essayHeaderBox ">
-                            <h1 className="essayHeader ">{EssayData[6].essayHeader}</h1>
-                        </div>
-
-                        <img
-                            className="objectExitButton cursorPoint"
-                            src="http://ellengallery.concordia.ca/establishing-a-database-connection/static/media/exit_Button.svg"
-                            alt=""
-                            onClick={this.props.toggleCopyright}
-                        ></img>
-                        <div className="copyrightBodyBox">
-                            <div className="essayBody">
-                                <p><i>{EssayData[6].disclaimer}</i></p>
-                                <br />
-                                <p>__________</p>
-                                <br />
-                                <p>{EssayData[6].essayText[0]}</p>
-
-                                <br />
-                                <p>{EssayData[6].essayText[1]}</p>
-
-                                <br />
-                                <p>{EssayData[6].essayText[2]}</p>
-
-                                <br />
-                                <p>{EssayData[6].essayText[3]}</p>
-
-                                <a
-                                    className="downloadBox cursorPoint"
-                                    href="http://ellengallery.concordia.ca/establishing-a-database-connection/static/media/EDC_Essay.pdf"
-                                    target="_blank">
-                                    View Full Text </a>
-
-                            </div>
-
-                        </div>
-                        <div className="essayFootnoteBox">
-                            <p>{EssayData[6].footnote[0]}</p>
-
-                            <p>{EssayData[6].footnote[1]}</p>
-
-                            <p>{EssayData[6].footnote[2]}</p>
-                            <div className="whiteOverlay"></div>
-                        </div>
-                    </div>
-                </div>
-            // </fragment>
+            <div className="copyrightContainer">
+                {this.getLanguage()}
+            </div>
         );
 
     }
@@ -126,6 +139,6 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {
-    // toggleLanguage, 
+    toggleLanguage,
     toggleCopyright,
 })(CopyrightWindow);
